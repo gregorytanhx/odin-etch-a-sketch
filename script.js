a5c7cb379ade61625e0afb6d6e5c25c0b1b1a69d
gregorytanhx <48156960+gregorytanhx@users.noreply.github.com>
2025-01-02 12:53:25 +0800

    adds function to only draw when mouse is held down

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+var color = '#D2691E';
+var draw = false;
 
 function hoverFunction(event) {
     // changes grid square when mouse hovers over it
-    event.target.style.background = "blue";
+    if (draw) {
+        event.target.style.background = color;
+    }
+}
+
+function mouseDown(event) {
+    draw = true;
+}
+
+function mouseUp(event) {
+    draw = false;
 }
 
 function createGrid(size) {
     const container = document.querySelector('.container');
+    container.addEventListener('mousedown', mouseDown);
+    container.addEventListener('mouseup', mouseUp);
     for (let i = 0; i < size; i++) {
-        const row = document.createElement("div");
-        row.classList.add("row");
+        const row = document.createElement('div');
+        row.classList.add('row');
         for (let j = 0; j < size; j++) {
             const div = document.createElement('div');
             div.classList.add('gridSquare');
 
-            div.addEventListener("mouseover", hoverFunction)
+            div.addEventListener('mouseover', hoverFunction);
 
             row.appendChild(div);
         }
-        container.appendChild(row);       
-
+        container.appendChild(row);
     }
 }
 
